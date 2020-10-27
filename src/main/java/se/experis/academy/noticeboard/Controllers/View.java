fd4b2f6e65fea5b474ff475f7f28d39f738c4f4a
Aman Zadran <amanzadran9@gmail.com>
2020-10-27 13:15:45 +0100

    feat(frontned): frontend implemented

---
+++ src/main/java/se/experis/academy/noticeboard/Controllers/View.java
@@ -0,0 +1,48 @@
+package se.experis.academy.noticeboard.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/")
+public class View {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index() {
+
+        return "index.html";
+    }
+
+    @RequestMapping(value = "post", method = RequestMethod.GET)
+    public String getPost() {
+        return "post.html";
+    }
+
+    @RequestMapping(value = "addpost", method = RequestMethod.GET)
+    public String addPost() {
+        return "addpost.html";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String login() {
+        return "login.html";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String register() {
+        return "register.html";
+    }
+
+    // not working...
+    @RequestMapping(value = "post/{id}", method = RequestMethod.GET)
+    public String singlePost(@PathVariable Integer id) {
+        System.out.println("here");
+        return "post.html";
+    }
+
+
+}
