86ef6083e4fe10939f2a64ec6610da2a9096423f
Emil Oja <emil.oja@gmail.com>
2020-10-29 09:30:13 +0100

    add jedis getConnection()

--- src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
+++ src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
@@ -4,11 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPoolConfig;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RedisConfig {
 
+    private static Jedis getConnection() throws URISyntaxException {
+        URI redisURI = new URI(System.getenv("REDIS_URL"));
+        Jedis jedis = new Jedis(redisURI);
+        return jedis;
+    }
     @Bean
     public RedisConnectionFactory jedisConnectionFactory(){
         JedisPoolConfig poolConfig = new JedisPoolConfig();
