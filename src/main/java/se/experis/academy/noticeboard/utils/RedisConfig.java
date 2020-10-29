684128a4d00d6fe5a3e8598ce8f6e3b8052eb64c
Emil Oja <emil.oja@gmail.com>
2020-10-29 11:08:15 +0100

    jedisconnectionfactory

--- src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
+++ src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
@@ -1,8 +1,10 @@
 package se.experis.academy.noticeboard.utils;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -11,17 +13,24 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
+@PropertySource("application.properties")
 public class RedisConfig {
+    @Value("${redis.hostname}")
+    private String redisHostName;
+
+    @Value("${redis.port}")
+    private int redisPort;
 
     @Bean
     public JedisPool getJedisPool() {
         try {
             URI redisURI = new URI("redis://h:p05678bbf8d604efc616a656fbab1df89041e9b59eaccb501bccaf19993b37052@ec2-34-239-50-117.compute-1.amazonaws.com:14079");
             return new JedisPool(new JedisPoolConfig(),
-                    redisURI.getHost(),
-                    redisURI.getPort(),
+                    redisHostName,
+                    redisPort,
                     Protocol.DEFAULT_TIMEOUT,
-                    redisURI.getUserInfo().split(":",2)[1]);
+//                    redisURI.getUserInfo().split(":",2)[1]);
+                    redisHostName,redisPort);
         } catch (URISyntaxException e) {
             throw new RuntimeException();
         }
