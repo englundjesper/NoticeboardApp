98d73dc3bc5ac1cecf030d7d5f5619c4b4d7f196
Emil Oja <emil.oja@gmail.com>
2020-10-29 11:17:49 +0100

    jedisconnectionfactory

--- src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
+++ src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
@@ -1,10 +1,8 @@
 package se.experis.academy.noticeboard.utils;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -13,19 +11,12 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
-@PropertySource("application.properties")
 public class RedisConfig {
-    @Value("${redis.hostname}")
-    private String redisHostName;
-
-    @Value("${redis.port}")
-    private int redisPort;
 
     @Bean
     public JedisPool getJedisPool() {
         try {
             URI redisURI = new URI("redis://h:p05678bbf8d604efc616a656fbab1df89041e9b59eaccb501bccaf19993b37052@ec2-34-239-50-117.compute-1.amazonaws.com:14079");
-            System.out.println("property port" + redisPort);
             return new JedisPool(new JedisPoolConfig(),
                     redisURI.getHost(),
                     redisURI.getPort(),
