3295f0c950f2ece8f55d8ac43d023a1de6fda4d0
Emil Oja <emil.oja@gmail.com>
2020-10-29 09:59:48 +0100

    add getPool on startup

--- src/main/java/se/experis/academy/noticeboard/NoticeboardAppApplication.java
+++ src/main/java/se/experis/academy/noticeboard/NoticeboardAppApplication.java
@@ -2,12 +2,18 @@ package se.experis.academy.noticeboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import se.experis.academy.noticeboard.utils.RedisConfig;
+
+import javax.swing.*;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class NoticeboardAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         SpringApplication.run(NoticeboardAppApplication.class, args);
+       RedisConfig.getPool();
     }
 
 }
