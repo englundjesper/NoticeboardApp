ec0a8377a497ea3ba603e219121ba2fc9c2cea9e
Emil Oja <emil.oja@gmail.com>
2020-10-29 11:14:53 +0100

    jedisconnectionfactory

--- src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
+++ src/main/java/se/experis/academy/noticeboard/utils/RedisConfig.java
@@ -26,11 +26,10 @@ public class RedisConfig {
         try {
             URI redisURI = new URI("redis://h:p05678bbf8d604efc616a656fbab1df89041e9b59eaccb501bccaf19993b37052@ec2-34-239-50-117.compute-1.amazonaws.com:14079");
             return new JedisPool(new JedisPoolConfig(),
-                    redisHostName,
+                    redisURI.getHost(),
                     redisPort,
                     Protocol.DEFAULT_TIMEOUT,
                     redisURI.getUserInfo().split(":",2)[1]);
-
         } catch (URISyntaxException e) {
             throw new RuntimeException();
         }
